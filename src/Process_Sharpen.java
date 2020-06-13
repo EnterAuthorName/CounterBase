930c9eceb3602c6ca131c6a13758934f2f7ec15b
Devon (PC) <reece.devon@gmail.com>
2020-06-14 02:06:47 +1200

    Completed the temppalte

--- src/Process_Sharpen.java
+++ src/Process_Sharpen.java
@@ -1,12 +1,27 @@
 import java.awt.image.BufferedImage;
+import java.util.Observable;
 
 /*
 One of the image processes that can happen.
 It sharpens the image
  */
-public class Process_Sharpen {
-    public BufferedImage process(BufferedImage img){
+public class Process_Sharpen extends Observable {
+    public void Process(BufferedImage img){
+        //add the image at start of process
+        notifyImageUpdate(img);
 
-        return null;
+        //implment the sharpening process
+
+        //add image at the end of the process
+        notifyImageUpdate(img);
+    }
+
+    /**
+     * Notifies any observers that the processor has updated a BufferedImage
+     * @param bufImage
+     */
+    private void notifyImageUpdate(BufferedImage bufImage){
+        setChanged();
+        notifyObservers(bufImage);
     }
 }
