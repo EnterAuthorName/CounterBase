d672bde335c3de5b966a1c2773703741a792e6ca
Devon (PC) <reece.devon@gmail.com>
2020-06-14 00:44:09 +1200

    Made a brance state And made a new class for branch state.
    New class returns null.

---
+++ src/Process_Sharpen.java
@@ -0,0 +1,12 @@
+import java.awt.image.BufferedImage;
+
+/*
+One of the image processes that can happen.
+It sharpens the image
+ */
+public class Process_Sharpen {
+    public BufferedImage process(BufferedImage img){
+
+        return null;
+    }
+}
