2f6096ae5bf8bd4931a635c5422e94b95798ba81
Devon (PC) <reece.devon@gmail.com>
2020-06-14 02:40:47 +1200

    Turns out I needed a image buffer for many of my actoins, so making it now is easier than making each time its required.

--- src/Counter.java
+++ src/Counter.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 /**
@@ -18,6 +19,12 @@ public class Counter {
             //setup Gui
             GUI gui = new GUI();
 
+            //setup buffereed image
+            // this is easier to do at the start and keep updated rather than having to do each process
+            //RGBA, Height, width
+            int[][][] rgba_buffer = new int[3][_bufImage.getHeight()][_bufImage.getWidth()];
+            rgba_buffer = getBuffer(rgba_buffer, _bufImage);
+
             //Create the auxiliary classes
             Processor processor = new Processor();
             Process_Sharpen sharpen = new Process_Sharpen();
@@ -33,6 +40,20 @@ public class Counter {
             System.out.println("[EXCEPTION] An error has occurred: " + e);
             e.printStackTrace();
         }
+    }
+
+    public static int[][][] getBuffer(int[][][] x, BufferedImage img){
+        //for each pixel
+        for(int row=0;row>img.getHeight();row++){
+            for(int col=0;col>img.getWidth();col++){
+                //get pixel color
+                Color c= new Color(img.getRGB(col,row));
+                x[0][row][col]=c.getRed();
+                x[1][row][col]=c.getGreen();
+                x[2][row][col]=c.getBlue();
+            }
+        }
+        return x;
     }
 
 }
