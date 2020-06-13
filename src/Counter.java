0a063a61a9b4689dcb5d3c12a3f9710d012986d4
Devon (PC) <reece.devon@gmail.com>
2020-06-14 03:13:25 +1200

    undid buffered input stuff

--- src/Counter.java
+++ src/Counter.java
@@ -19,12 +19,6 @@ public class Counter {
             //setup Gui
             GUI gui = new GUI();
 
-            //setup buffereed image
-            // this is easier to do at the start and keep updated rather than having to do each process
-            //RGBA, Height, width
-            int[][][] rgba_buffer = new int[3][_bufImage.getHeight()][_bufImage.getWidth()];
-            rgba_buffer = getBuffer(rgba_buffer, _bufImage);
-
             //Create the auxiliary classes
             Processor processor = new Processor();
             Process_Sharpen sharpen = new Process_Sharpen();
@@ -34,7 +28,7 @@ public class Counter {
 
             //do processes
             //sharpen
-            sharpen.Process(_bufImage,rgba_buffer);
+            sharpen.Process(_bufImage);
             processor.Process(_bufImage);
         }
         catch (Exception e){
@@ -42,19 +36,4 @@ public class Counter {
             e.printStackTrace();
         }
     }
-
-    public static int[][][] getBuffer(int[][][] x, BufferedImage img){
-        //for each pixel
-        for(int row=0;row>img.getHeight();row++){
-            for(int col=0;col>img.getWidth();col++){
-                //get pixel color
-                Color c= new Color(img.getRGB(col,row));
-                x[0][row][col]=c.getRed();
-                x[1][row][col]=c.getGreen();
-                x[2][row][col]=c.getBlue();
-            }
-        }
-        return x;
-    }
-
 }
